920121a9d88ed69015e96dbfcd3c1d106f7e3969
Streafe <streafe@gmail.com>
2022-03-15 12:16:55 +0100

    Added further necessary files

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms512M -Xmx512M -jar spigot-1.18.2.jar
+java -Xmx2048M -Xmx2048M -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+ZProactive -XX:ZCollectionInterval=600 -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -jar spigot-1.18.2.jar nogui
